f955aaa383b800e90c273c3cc2ea9e76e77277e5
Dngn20 <dngu9879@740s-pl-998.local>
2016-10-25 13:23:25 -0600

    Added init and data members

--- CardGame/Card.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Card.swift
-//  CardGame
-//
-//  Created by Nguyen, Duncan on 10/25/16.
-//  Copyright © 2016 Nguyen, Duncan. All rights reserved.
-//
-
-import Foundation
---
+++ CardGame/Model/Card.swift
@@ -0,0 +1,22 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Nguyen, Duncan on 10/25/16.
+//  Copyright © 2016 Nguyen, Duncan. All rights reserved.
+//
+
+import UIKit
+
+class Card
+{
+    private var backImage : UIImage
+    private var isFaceUP : Bool
+    
+    init()
+    {
+        backImage = UIImage()
+        isFaceUP = false
+        
+    }
+}
