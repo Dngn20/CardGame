33cfbe2f608a235e04ed403c45dc4a72f56b2cba
Dngn20 <dngu9879@740s-pl-998.local>
2016-10-25 13:15:44 -0600

    Added Card Classes to Xcode

---
+++ CardGame/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Nguyen, Duncan on 10/25/16.
+//  Copyright © 2016 Nguyen, Duncan. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Nguyen, Duncan on 10/25/16.
+//  Copyright © 2016 Nguyen, Duncan. All rights reserved.
+//
+
+import Foundation
