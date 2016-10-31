9c405fa3483f921e9698fff21f4eeff5b7998aa6
Dngn20 <dngu9879@740s-pl-998.local>
2016-10-31 13:00:08 -0600

    Added Helper methods to make sure we have the ranks and suits used

--- CardGame/PlayingCard.swift
+++ CardGame/PlayingCard.swift
@@ -77,4 +77,23 @@ class PlayingCard : Card
         return description
     }
 
+    //The Class mdifeir makes it soe the method is visable without an instance 
+    //You would call it ba ClassName,method()
+    //In this case PlayingCard.validranks()
+    class func validRanks() -> [String]
+    {
+    return ["??","A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    }
+
+
+    class func maxRank() -> Int
+    {
+        return validRanks().count - 1
+    }
+    
+    class func validSuit() -> [String]
+    {
+        return ["♣️","♥️","♠️","♦️"]
+    }
+    
 }
