24acebefe2e7ab85cd12446785ceb8d1038c07b9
Dngn20 <dngu9879@740s-pl-998.local>
2016-11-09 12:08:11 -0700

    Added a shuffle to the Deck

--- CardGame/Deck.swift
+++ CardGame/Deck.swift
@@ -14,7 +14,24 @@ class Deck
     
     func shffleDeck() -> Void
     {
+        //Created a temp variable to hold Cards.
+        var tempDeck = [Card]()
         
+        //Repeat until cards data members is empty
+        while self.cards.count > 0
+        {
+            //get a rendom number betwwen 0...cards.count -1
+            let randomIndex = Int (arc4random() % (UInt32)(cards.count))
+            
+            //Remove the card at that index from the deck.
+            let removedCard = cards.removeAtIndex(randomIndex)
+            
+            //Place the data member with the updated data
+            tempDeck.append(removedCard)
+        }
+        
+        //Replace the data member with the updated data
+        self.cards = tempDeck
     }
 
     func cutDeck() -> Void
